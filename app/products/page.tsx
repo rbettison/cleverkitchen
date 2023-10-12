6a1cc68fe2d0ee71762d55f59df86902f4a70920
Rob Bettison <rbettison>
2023-10-12 19:37:08 +0200

    Getting the first 5 products for a store (if we have the env variable)

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,41 @@
-export default function Products() {
+export default async function Products() {
+    let products = [];
+
+    let query = `{
+        products(first: 5) {
+          edges {
+              node {
+                  title
+              }
+          }
+        }
+      }`
+    console.log(process.env.SHOPIFY_GRAPHQL);
+    if(process.env.SHOPIFY_GRAPHQL != undefined) {
+        let variables= {};
+        let response = await fetch(
+            process.env.SHOPIFY_GRAPHQL,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Shopify-Storefront-Access-Token': process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN
+                },
+                body: JSON.stringify({ query, variables })
+            }
+        );
+        console.log(response);
+        products = await response.json();
+        console.log(products);
+    }
+
+    
+
     return (
         <>
             <h1>All Products</h1>
             <h2>Here, you'll see lots of cool products.</h2>
+            {products.data.products.edges.map((edge: any) => <h3 key={edge.node.handle}>{edge.node.title}</h3>)}
         </>
     )
 }
