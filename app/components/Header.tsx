8a2999a222fd39f36d636467c98f213a6ef25186
Rob Bettison <rbettison>
2023-10-19 16:08:26 +0200

    Clever Kitchen header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,6 +10,7 @@ export default function Header() {
 
     return (
         <div className="flex flex-col justify-center items-center pt-4 pb-4 gap-3">
+            <h1 className="uppercase font-extrabold text-4xl text-center">Clever Kitchen</h1>
             <Link href="/cart"><CartIcon number={cartItems.length}/></Link>
             <Link href="/" className="hover:underline">Home</Link>
             <Link href="/products" className="hover:underline">Products</Link>
