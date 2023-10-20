861dcc6ffe7896fa3599fb3a3c6a801f389dd6f3
Rob Bettison <rbettison>
2023-10-20 10:15:10 +0200

    hover effects and others

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,11 +9,19 @@ export default function Header() {
     const { cartItems } = useContext(CartContext)
 
     return (
-        <div className="flex flex-col justify-center items-center pt-4 pb-4 gap-3">
-            <h1 className="uppercase font-extrabold text-4xl text-center">Clever Kitchen</h1>
+        <div className="flex flex-col items-end pt-4 pb-4 gap-3 text-right">
+            <div>
+            <h1 className="uppercase font-extrabold text-4xl">Clever Kitchen</h1>
+            </div>
+            <div>
             <Link href="/cart"><CartIcon number={cartItems.length}/></Link>
-            <Link href="/" className="hover:underline">Home</Link>
-            <Link href="/products" className="hover:underline">Products</Link>
+            </div>
+            <div>
+            <Link href="/" className="hover:underline text-sm">Home</Link>
+            </div>
+            <div>
+            <Link href="/products" className="hover:underline text-sm">Products</Link>
+            </div>
         </div>
     )
 }
--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -8,14 +8,14 @@ const currencies = {
 export default function ProductCard(props: {handle: string, title: string, image: string, price: number, currencyCode: string}) {
     let imageUrl = props.image;
     return (
-        <div className="box-border rounded-md overflow-hidden shadow-md">
+        <div className="box-border rounded-md overflow-hidden shadow-sm hover:shadow-lg">
                 <div className="bg-white h-full flex flex-col">
                     
                     <img src={imageUrl} className="w-full h-40 min-h-32 object-cover"/>
                     <div className="p-4 flex flex-col justify-between h-full items-center">
                         <span className="text-secondary-200 text-l font-light text-center" key={props.handle}>{props.title}</span>
                         <span className="text-secondary-200 block font-light">{currencies.GBP + props.price}</span>
-                        <span className="bg-primary-100 p-1 rounded-full text-white text-sm mt-3"><Link href={`/products/${props.handle}`}>More</Link></span>
+                        <span className="bg-primary-100 p-1 rounded-full text-white text-sm mt-3 hover:bg-white hover:text-primary-200 border"><Link href={`/products/${props.handle}`}>More</Link></span>
 
                     </div>
                     
