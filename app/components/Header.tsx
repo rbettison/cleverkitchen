2e2a19beecb80b454ccf95d451f700b2994b2901
Tim <tbettison@deloitte.co.uk>
2023-11-07 11:28:42 +0000

    reset

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,19 +9,6 @@ export default function Header() {
     const { cartItems } = useContext(CartContext)
 
     return (
-        <div className="flex flex-col items-end pt-4 pb-4 gap-3 text-right">
-            <div>
-            <h1 className="uppercase font-extrabold text-4xl">Clever Kitchen</h1>
-            </div>
-            <div>
-            <Link href="/cart"><CartIcon number={cartItems.length}/></Link>
-            </div>
-            <div>
-            <Link href="/" className="hover:underline text-sm">Home</Link>
-            </div>
-            <div>
-            <Link href="/products" className="hover:underline text-sm">Products</Link>
-            </div>
-        </div>
+        <p>hi!</p>
     )
 }
