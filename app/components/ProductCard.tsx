f8bda94af029b213899577ef8ebf30af97e5b616
rbettison <rob.bettison94@gmail.com>
2024-02-16 20:24:43 +0100

    product card fix

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -18,7 +18,7 @@ export default function ProductCard(props: {handle: string, title: string, image
             href={`/products/${props.handle}`} className="box-border overflow-hidden shadow-sm hover:shadow-lg rounded-lg border border-border-100">
                 <div className="bg-white h-full flex flex-col ">
                     
-                    <img src={imageUrl} className="w-full object-cover"/>
+                    <img src={imageUrl} className="w-full h-[500px] object-cover"/>
                     <div className="p-4 flex flex-col justify-between h-full items-center border-t border-border-100">
                         <span className="text-l font-light text-center text-[20px]" key={props.handle}>{props.title}</span>
                         <span className="block font-light text-[24px] text-price-100">{currencies.GBP + props.price}</span>
