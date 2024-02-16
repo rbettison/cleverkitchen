facb2dc2e7a867b63925e98e114a0bbb20b9cc35
rbettison <rob.bettison94@gmail.com>
2024-02-16 20:09:16 +0100

    checkout button fix

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -4,7 +4,7 @@ import OrderSummary from "./components/OrderSummary";
 export default function Cart() {
     return (
         <>
-            <div className="pb-4 pl-4 pr-4 pt-40 sm:pt-24">
+            <div className="pb-52 pl-4 pr-4 pt-40 sm:pt-24">
             <p className="font-bold text-3xl mb-4">Your Cart</p>
             <div className="sm:grid flex flex-col grid-cols-2">
                 <CartItems />
